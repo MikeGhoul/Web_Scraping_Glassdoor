545b3d10ed63ec5c2b613124fe19823494379b99
MikeGhoul <mike.ghoul5@gmail.com>
2017-08-06 11:45:59 -0400

    Wordcloud WF Cons

---
+++ wordcloud_Cons_WF.R
@@ -0,0 +1,43 @@
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+
+text <- readLines(file.choose('Cons_WF.txt'))
+docs <- Corpus(VectorSource(text))
+inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+docs <- tm_map(docs, content_transformer(tolower))
+
+docs <- tm_map(docs, removeNumbers)
+
+docs <- tm_map(docs, removeWords, stopwords("english"))
+ 
+docs <- tm_map(docs, removeWords, c("wells", "can", "fargo", "get","will", "like", "make", "many", 
+                                    "even","lot", "just", "day", "one", "really", "well", "work"))
+docs <- tm_map(docs, removePunctuation)
+
+docs <- tm_map(docs, stripWhitespace)
+
+
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=150, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
